4ac52cb87faac464e4ab9a6b88e5e88d876c622b
CharltonAwesome1 <charltonawesome@gmail.com>
2023-09-15 02:03:04 +0200

    General updates
     Updated site title
     Changed About button to hide on scroll down
     I think that's it

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,15 +1,45 @@
 import { Outlet, Link } from "react-router-dom";
 import './App.css';
+import { useState, useEffect, useRef, useCallback } from "react";
+// import { useEffect, useRef  } from "react";
+// import $ from 'jquery';
 
 const Layout = () => {
+
+  const count = useRef("notHidden");
+  const [y, setY] = useState(window.scrollY);
+
+  const handleNavigation = useCallback(
+    e => {
+      const window = e.currentTarget;
+      if (y > window.scrollY) {
+        count.current = "notHidden";
+      } else if (y < window.scrollY) {
+        count.current = "hidden";
+      }
+      setY(window.scrollY);
+    }, [y]
+  );
+
+  useEffect(() => {
+    setY(window.scrollY);
+    window.addEventListener("scroll", handleNavigation);
+
+    return () => {
+      window.removeEventListener("scroll", handleNavigation);
+    };
+  }, [handleNavigation]);
+
+  // https://sentry.io/answers/unique-key-prop/
+
   return (
     <>
       <nav>
         <ul >
-          <li>
+          <li >
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li >
             <Link to="/skillshowcase">Skill Showcase</Link>
           </li>
           <li>
@@ -17,8 +47,9 @@ const Layout = () => {
           </li>
         </ul>
       </nav>
-      
-      <button className='aboutBtn'>About</button>
+      <div className={count.current} >
+        <button className='aboutBtn'>About</button>
+      </div>
 
       <Outlet />
     </>
--- src/pages/SkillShowcase.js
+++ src/pages/SkillShowcase.js
@@ -15,57 +15,80 @@ const SkillShowcase = () => {
         .then((res) => res.json())
         .then((data) => setData(data));
     }, []);
-  
+
     useEffect(() => {
       fetch("https://jsonplaceholder.typicode.com/photos?albumId=1")
         .then((res) => res.json())
         .then((imgData) => setImageData(imgData));
     }, []);
-  
-  
+
+    // const [count, setCount] = useState(0);
+
+    // useEffect(() => {
+    //   setTimeout(() => {
+    //     setCount((count) => count + 1);
+    //     alert(window.scrollY)
+    //   }, 1000);
+    // });
+
+    // window.addEventListener ('scroll', (event) => {
+    //   console.log(event);
+
+    // });
+
+    /*useEffect(() => {
+      // onScroll= console.log(window.scrollY);
+      window.onscroll (() => {
+          console.log("We did it, Joe.")
+      })
+      });*/
+    // });
+    
+    // window.scrollY
+
+
+
+
     return (
       <>
-        {/* <br /> */}
-        {/* <div className='outsideDiv'> */}
-          <div className='outsideDiv'>
-            <h1>Skill Showcase</h1>
-          </div>
-        {/* </div> */}
-        <div className='skillsText'>
-        <div className='directionBtn'>
-          <button className='prevBtn' onClick={() => setCounter((c) => c > 0 ? c - 1 : 0)}>Previous</button>
-          <button className='nextBtn' onClick={() => setCounter((c) => data.length / 10 > c + 1 ? c + 1 : data.length / 10 - 1)}>Next</button>
+        <div className='outsideDiv'>
+          <h1>Skill Showcase</h1>
         </div>
-        <div className='leftText'>
-        {data &&
-          data.map((item) => {
-            return (
-              (item.id >= (counter * 10 + 1) && item.id <= (counter * (10) + 10)) ?
-                (<Fade left><p className='floatText' key={item.id}>{item.id} .  {item.title}</p></Fade>) :
-                <></>
-            )
-          })};
+        <div className='skillsText'>
+          <div className='directionBtn'>
+            <button className='prevBtn' onClick={() => setCounter((c) => c > 0 ? c - 1 : 0)}>Previous</button>
+            <button className='nextBtn' onClick={() => setCounter((c) => data.length / 10 > c + 1 ? c + 1 : data.length / 10 - 1)}>Next</button>
           </div>
-      </div>
+          <div className='leftText'>
+            {data &&
+              data.map((item) => {
+                return (
+                  (item.id >= (counter * 10 + 1) && item.id <= (counter * (10) + 10)) ?
+                    (<Fade left><p className='floatText' key={item.id}>{item.id} .  {item.title}</p></Fade>) :
+                    <></>
+                )
+              })};
+          </div>
+        </div>
 
-      <button onClick={() => setImageCounter((c) => c > 0 ? c - 1 : 0)}>Previous image page</button>
-      <button onClick={() => setImageCounter((c) => imgData.length / 10 > c + 1 ? c + 1 : imgData.length / 10 - 1)}>Next image page</button>
-      <div>
-        {imgData &&
-          imgData.map((image) => {
-            return (
-              (image.id >= (imageCounter * 10 + 1) && image.id <= (imageCounter * (10) + 10)) ?
-                (
-                  <Bounce>
-                    <div className='test'>
-                      <img key={image.id} src={image.thumbnailUrl} alt={image.title} />
-                    </div>
-                  </Bounce>
-                ) :
-                <></>
-            )
-          })};
-      </div>
+        <button onClick={() => setImageCounter((c) => c > 0 ? c - 1 : 0)}>Previous image page</button>
+        <button onClick={() => setImageCounter((c) => imgData.length / 10 > c + 1 ? c + 1 : imgData.length / 10 - 1)}>Next image page</button>
+        <div>
+          {imgData &&
+            imgData.map((image) => {
+              return (
+                (image.id >= (imageCounter * 10 + 1) && image.id <= (imageCounter * (10) + 10)) ?
+                  (
+                    <Bounce>
+                      <div className='test'>
+                        <img key={image.id} src={image.thumbnailUrl} alt={image.title} />
+                      </div>
+                    </Bounce>
+                  ) :
+                  <></>
+              )
+            })};
+        </div>
       </>);
   }
 };
